51806dc8a187f9c8cbbe0c871bc50fb665339611
Anruag Parida <anuragparida37@gmail.com>
2021-10-23 10:29:47 +0530

    Dash Numbers

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,5 +1,6 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { server, config, checkAccess } from "../../env";
 import Footer from "./../../components/Footer/Footer";
 import Sidebar from "./../../components/Sidebar";
 import Header from "./../../components/Header/Header";
@@ -8,79 +9,106 @@ import DashLineGraph from "../../components/DashLineGraph/DashLineGraph";
 import DashDonutGraph from "./../../components/DashDonutGraph/DashDonutGraph";
 import DashStatsCard from "../../components/DashStatsCard/DashStatsCard";
 
-const Dashboard = () => (
-  <div class="nk-app-root">
-    <div class="nk-main ">
-      <Sidebar />
-      <div class="nk-wrap">
-        <Header />
-        <div class="nk-content ">
-          <div class="container-fluid">
-            <div class="nk-content-inner">
-              <div class="nk-content-body">
-                <div class="nk-block-head nk-block-head-sm">
-                  <div class="nk-block-between">
-                    <div class="nk-block-head-content">
-                      <h3 class="nk-block-title page-title">Dashboard</h3>
-                    </div>
-                    <div class="nk-block-head-content">
-                      <div class="toggle-wrap nk-block-tools-toggle">
-                        <div
-                          class="toggle-expand-content"
-                          data-content="pageMenu"
-                        >
-                          <ul class="nk-block-tools g-3">
-                            <li>
-                              <div class="drodown">
-                                <a
-                                  class="dropdown-toggle btn btn-white btn-dim btn-outline-light"
-                                  href="#"
-                                  data-toggle="dropdown"
-                                >
-                                  <span class="d-none d-md-inline">Last</span>
-                                  30 Days
-                                </a>
-                                <div class="dropdown-menu dropdown-menu-right">
-                                  <ul class="link-list-opt no-bdr">
-                                    <li>
-                                      <a href="#">Last 30 Days</a>
-                                    </li>
-                                    <li>
-                                      <a href="#">Last 6 Months</a>
-                                    </li>
-                                    <li>
-                                      <a href="#">Last 1 Years</a>
-                                    </li>
-                                  </ul>
+const Dashboard = () => {
+  const [numberData, setNumberData] = useState({});
+
+  const updateNumberData = async () => {
+    await axios
+      .get(server + "/api/admin/dashboard", config)
+      .then((rsp) => {
+        console.log(rsp);
+        setNumberData(rsp.data.payload);
+      })
+      .catch((err) => {
+        checkAccess(err);
+        console.error(err);
+      });
+  };
+
+  useEffect(() => updateNumberData(), []);
+
+  return (
+    <div class="nk-app-root">
+      <div class="nk-main ">
+        <Sidebar />
+        <div class="nk-wrap">
+          <Header />
+          <div class="nk-content ">
+            <div class="container-fluid">
+              <div class="nk-content-inner">
+                <div class="nk-content-body">
+                  <div class="nk-block-head nk-block-head-sm">
+                    <div class="nk-block-between">
+                      <div class="nk-block-head-content">
+                        <h3 class="nk-block-title page-title">Dashboard</h3>
+                      </div>
+                      <div class="nk-block-head-content">
+                        <div class="toggle-wrap nk-block-tools-toggle">
+                          <div
+                            class="toggle-expand-content"
+                            data-content="pageMenu"
+                          >
+                            <ul class="nk-block-tools g-3">
+                              <li>
+                                <div class="drodown">
+                                  <a
+                                    class="dropdown-toggle btn btn-white btn-dim btn-outline-light"
+                                    href="#"
+                                    data-toggle="dropdown"
+                                  >
+                                    <span class="d-none d-md-inline">Last</span>
+                                    30 Days
+                                  </a>
+                                  <div class="dropdown-menu dropdown-menu-right">
+                                    <ul class="link-list-opt no-bdr">
+                                      <li>
+                                        <a href="#">Last 30 Days</a>
+                                      </li>
+                                      <li>
+                                        <a href="#">Last 6 Months</a>
+                                      </li>
+                                      <li>
+                                        <a href="#">Last 1 Years</a>
+                                      </li>
+                                    </ul>
+                                  </div>
                                 </div>
-                              </div>
-                            </li>
-                            <li class="nk-block-tools-opt">
-                              <a class="btn btn-primary" href="#">
-                                Reports
-                              </a>
-                            </li>
-                          </ul>
+                              </li>
+                              <li class="nk-block-tools-opt">
+                                <a class="btn btn-primary" href="#">
+                                  Reports
+                                </a>
+                              </li>
+                            </ul>
+                          </div>
                         </div>
                       </div>
                     </div>
                   </div>
-                </div>
-                <div class="nk-block">
-                  <div class="row g-gs">
-                    <DashNumberCard />
-                    <DashNumberCard />
-                    <DashNumberCard />
-                    <DashNumberCard />
+                  <div class="nk-block">
+                    <div class="row g-gs">
+                      {numberData && (
+                        <>
+                          <DashNumberCard data={["Users", numberData.users]} />
+                          <DashNumberCard
+                            data={["Orders", numberData.orders]}
+                          />
+                          <DashNumberCard
+                            data={["Revenue", numberData.revenue]}
+                          />
+                          <DashNumberCard
+                            data={["Orders", numberData.orders]}
+                          />
+                        </>
+                      )}
+                      <DashLineGraph />
+                      <DashDonutGraph />
+                      <DashStatsCard />
 
-                    <DashLineGraph />
-                    <DashDonutGraph />
-                    <DashStatsCard />
-
-                    <DashStatsCard />
-                    <DashLineGraph />
-                    <DashDonutGraph />
-                    {/* <div class="col-xxl-8">
+                      <DashStatsCard />
+                      <DashLineGraph />
+                      <DashDonutGraph />
+                      {/* <div class="col-xxl-8">
                       <div class="card card-full">
                         <div class="card-inner">
                           <div class="card-title-group">
@@ -348,20 +376,17 @@ const Dashboard = () => (
                         </div>
                       </div>
                     </div> */}
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
+          <Footer />
         </div>
-        <Footer />
       </div>
     </div>
-  </div>
-);
-
-Dashboard.propTypes = {};
-
-Dashboard.defaultProps = {};
+  );
+};
 
 export default Dashboard;
